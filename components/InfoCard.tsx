c9ecb9e4a01fcf5dd4f2a3ecd4e8d438409f7293
edthecodepusher <edthecodepusher@proton.me>
2024-11-13 22:42:55 +0000

    card update

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Image from "next/legacy/image";
+import Image from 'next/legacy/image';
 import Link from 'next/link';
 import { allPosts, Post } from 'contentlayer/generated';
 import { compareDesc, format, parseISO } from 'date-fns';
+import { getMDXComponent } from 'next-contentlayer/hooks';
 
 import {
   Card,
@@ -13,7 +14,8 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 
-export function InfoCard(post: Post) {
+function PostCard(post: Post) {
+  const Content = getMDXComponent(post.body.code);
   return (
     <Card>
       <Image
@@ -32,11 +34,55 @@ export function InfoCard(post: Post) {
           creatives.
         </CardContent>
 
- {/* <CardFooter>{format(parseISO(post.date), 'LLLL d, yyyy')}</CardFooter> */}
+        {/* <CardFooter>{format(parseISO(post.date), 'LLLL d, yyyy')}</CardFooter> */}
       </div>
       <CardButton href="/store" text="View" />
     </Card>
   );
 }
 
-export default InfoCard;
+export default PostCard;
+
+// import Link from "next/link";
+// import { compareDesc, format, parseISO } from "date-fns";
+// import { allPosts, Post } from "contentlayer/generated";
+// import { getMDXComponent } from "next-contentlayer/hooks";
+
+// function PostCard(post: Post) {
+//   const Content = getMDXComponent(post.body.code);
+
+//   return (
+//     <div className="mb-8">
+//       <h2 className="text-xl">
+//         <Link
+//           href={post.url}
+//           className="text-blue-700 hover:text-blue-900"
+//           legacyBehavior>
+//           {post.title}
+//         </Link>
+//       </h2>
+//       <time dateTime={post.date} className="block mb-2 text-xs text-gray-600">
+//         {format(parseISO(post.date), "LLLL d, yyyy")}
+//       </time>
+//       <div className="text-sm">
+//         <Content />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default function Home() {
+//   const posts = allPosts.sort((a, b) =>
+//     compareDesc(new Date(a.date), new Date(b.date))
+//   );
+
+//   return (
+//     <div className="max-w-xl py-8 mx-auto">
+//       <h1 className="mb-8 text-3xl font-bold text-center">Next.js Example</h1>
+
+//       {posts.map((post, idx) => (
+//         <PostCard key={idx} {...post} />
+//       ))}
+//     </div>
+//   );
+// }
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,23 +1,33 @@
 import * as React from 'react';
 import Link from 'next/link';
+import { allPosts, Post } from 'contentlayer/generated'; // Assuming Post is a type you want to use
+import { compareDesc, format, parseISO } from 'date-fns';
+import { getMDXComponent } from 'next-contentlayer/hooks';
 import { PiArrowSquareOutThin } from 'react-icons/pi';
 
 import { cn } from '@/lib/utils'; // Assuming you have a utility function `cn` for conditional classNames
 
 // Card component
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      'mt-8 flex justify-between gap-x-6 rounded-xl bg-[#161616] p-3',
-      className
-    )}
-    {...props}
-  />
-));
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  post: Post; // Define the type of 'post' prop here
+}
+
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, post, ...props }, ref) => (
+    <Link href={post.url}>
+      {' '}
+      {/* Use 'post.url' here */}
+      <div
+        ref={ref}
+        className={cn(
+          'mt-8 flex justify-between gap-x-6 rounded-xl bg-[#161616] p-3',
+          className
+        )}
+        {...props}
+      />
+    </Link>
+  )
+);
 Card.displayName = 'Card';
 
 // CardHeader component
